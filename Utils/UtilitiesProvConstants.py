d8bea05b1a5dc138103b8cfbe63379d6a0dd5d87
AndreAloise77 <44739910+AndreAloise77@users.noreply.github.com>
2021-02-03 20:42:19 -0300

    Creating Utilities Classes 02
    Criando classe utilitária que armazena alguns valores constantes utilizados para ler da árvore de proveniência.

---
+++ Utils/UtilitiesProvConstants.py
@@ -0,0 +1,18 @@
+class UtilitiesProvConstants:
+    # Vertices:
+    VERTICES = 'vertices'
+    VERTEX = 'vertex'
+    ID = 'ID'
+    TYPE = 'type'
+    LABEL = 'label'
+    VERTEX_DATE = 'date'
+    ATTRIBUTES = 'attributes'
+    ATTRIBUTE = 'attribute'
+    ATTRIBUTE_NAME = 'name'
+    VALUE = 'value'
+
+    # Edge:
+    EDGES = 'edges'
+    EDGE = 'edge'
+    EDGE_SOURCE_ID = 'sourceID'
+    EDGE_TARGET_ID = 'targetID'
