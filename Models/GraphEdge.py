b001fe0d800081cfd2ddae6bea99e351a0a537ea
AndreAloise77 <44739910+AndreAloise77@users.noreply.github.com>
2021-02-09 03:47:43 -0300

    Update GraphEdge Model wih Respawn attribute
    Atualizando clase de modelo para conter informação se a aresta (edge) do grafo é um Respawn

--- Models/GraphEdge.py
+++ Models/GraphEdge.py
@@ -4,7 +4,8 @@ from Models.GraphVertex import GraphVertex
 
 
 class GraphEdge:
-    def __init__(self, source_vertex_region: GraphVertex, target_vertex_region: GraphVertex, session_name: str):
+    def __init__(self, source_vertex_region: GraphVertex, target_vertex_region: GraphVertex,
+                 session_name: str, is_respawn: bool):
 
         self.source_vertex_region = source_vertex_region
         self.target_vertex_region = target_vertex_region
@@ -12,6 +13,7 @@ class GraphEdge:
         self.frequency = 1
         self.sessions: List[str] = []
         self.sessions.append(session_name)
+        self.is_respawn = is_respawn
 
     def print_edge(self):
         return 'NodeId: {}, Frequency: {}'.format(self.id_node, self.frequency)
