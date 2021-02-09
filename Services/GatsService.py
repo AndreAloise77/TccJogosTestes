b406d6ea477fce67e9907d615954fd04eb2be141
AndreAloise77 <44739910+AndreAloise77@users.noreply.github.com>
2021-02-09 03:51:28 -0300

    Refactor on GatsService methods
    Refatorando toda lógica dos métodos de serviço, focando agora somente na criação dos Gats
    
    Removendo código confuso e que não será mais atualizado

--- Services/GatsService.py
+++ Services/GatsService.py
@@ -9,58 +9,45 @@ from Models.GraphEdge import GraphEdge
 COLOR_GREEN = 'green'
 COLOR_BLUE = 'blue'
 COLOR_BLACK = 'black'
+COLOR_RED = 'red'
 
 
-def __create_dot_and_nodes(graph: Graph, file_name: str):
+def try_to_build_gats_graph(graph: Graph, file_name_list: List[str], loop: int, should_paint: bool):
+    dot = __create_dot_and_nodes_for_gats_graph(graph, file_name_list, should_paint)
+    common_edges: List[str] = __get_common_edges(graph, file_name_list, loop)
+    gats_edge_dict: Dict[str, GraphEdge] = graph.get_edges()
+    for key_value in gats_edge_dict:
+        id_node_value: str = key_value
+        gats_edge_vertex_regions: List[str] = id_node_value.split('_')
+        source_region: str = gats_edge_vertex_regions[0]
+        target_region: str = gats_edge_vertex_regions[1]
+        edge_frequency: int = gats_edge_dict[id_node_value].frequency
+        is_respawn = gats_edge_dict[id_node_value].is_respawn
+        if not should_paint:
+            __create_colored_dot_edge(dot, edge_frequency, source_region, target_region, COLOR_BLACK)
+        else:
+            if key_value in common_edges:
+                """Caminho em comum para todas as sessions"""
+                __create_colored_dot_edge(dot, edge_frequency, source_region, target_region, COLOR_BLUE, is_respawn)
+            elif key_value in graph.newEdgeElements:
+                """Caminho novo para a ultima session"""
+                __create_colored_dot_edge(dot, edge_frequency, source_region, target_region, COLOR_GREEN, is_respawn)
+            else:
+                """Somente um caminho que não é novo, comum ou respawn"""
+                __create_colored_dot_edge(dot, edge_frequency, source_region, target_region, COLOR_BLACK, is_respawn)
+    return dot
+
+
+def __create_dot_and_nodes_for_gats_graph(graph: Graph, file_name_list: List[str], should_paint: bool):
+    file_name: str = str(file_name_list)
     dot = Digraph(comment=file_name)
     node_name_list: List[str] = __get_node_name_form_graph(graph)
     for name_region in node_name_list:
-        if name_region in graph.newVertexElements:
+        if should_paint and (name_region in graph.newVertexElements):
             dot.node(name_region, name_region, style='filled', fillcolor=COLOR_GREEN)
         else:
             dot.node(name_region, name_region)
     return dot
-
-
-def __create_dot_and_nodes_for_multiple_sessions(graph: Graph, file_name: List[str]):
-    file: str = str(file_name)
-    return __create_dot_and_nodes(graph, file)
-
-
-def __create_dot_and_nodes_for_single_session(graph: Graph, file_name: str):
-    return __create_dot_and_nodes(graph, file_name)
-
-
-def __create_colored_dot_edge(dot: Digraph, frequency: int, node_a: str, node_b: str, edge_color: str):
-    frequency_text: str = "Freq: {}".format(frequency)
-    dot.edge(node_a, node_b, frequency_text,  color=edge_color, constraint='true')
-
-
-def try_to_build_gats_graphs_for_multiple(graph: Graph, file_name: List[str], loops: int):
-    dot = __create_dot_and_nodes_for_multiple_sessions(graph, file_name)
-    common_edges: List[str] = __get_common_edges_from_all_sessions(graph, file_name, loops)
-    edge_dict: Dict[str, GraphEdge] = graph.get_edges()
-    for key in edge_dict:
-        edge_regions: List[str] = key.split('_')
-        frequency: int = edge_dict[key].frequency
-        if key in common_edges:
-            __create_colored_dot_edge(dot, frequency, edge_regions[0], edge_regions[1], COLOR_BLUE)
-        elif key in graph.newEdgeElements:
-            __create_colored_dot_edge(dot, frequency, edge_regions[0], edge_regions[1], COLOR_GREEN)
-        else:
-            __create_colored_dot_edge(dot, frequency, edge_regions[0], edge_regions[1], COLOR_BLACK)
-    dot.render('test-output/{}.gv'.format(file_name), view=True)
-
-
-def try_to_build_gats_graphs_for_single(graph: Graph, file_name: str):
-    dot = __create_dot_and_nodes_for_single_session(graph, file_name)
-    edge_dict: Dict[str, GraphEdge] = graph.get_edges()
-    for key_value in edge_dict:
-        id_node_value: str = key_value
-        edge_regions: List[str] = id_node_value.split('_')
-        frequency: int = edge_dict[key_value].frequency
-        __create_colored_dot_edge(dot, frequency, edge_regions[0], edge_regions[1], COLOR_BLACK)
-    dot.render('test-output/{}.gv'.format(file_name), view=True)
 
 
 def __get_node_name_form_graph(graph: Graph):
@@ -71,15 +58,25 @@ def __get_node_name_form_graph(graph: Graph):
     return v_list
 
 
-def __get_common_edges_from_all_sessions(graph: Graph, file_name_list: List[str], loop: int = None):
+def __get_common_edges(graph: Graph, file_name_list: List[str], loop: int):
     edge_dict = graph.edges
     edge_list: List[str] = []
     for key_value in edge_dict:
         graph_edge = edge_dict[key_value]
         frequency: int = edge_dict[key_value].frequency
         if frequency >= loop:
-            file_name = file_name_list[loop-1]
+            file_name = file_name_list[loop - 1]
             if file_name in graph_edge.sessions:
                 edge_list.append(key_value)
     """End for"""
     return edge_list
+
+
+def __create_colored_dot_edge(dot: Digraph, frequency: int, source_node: str, target_node: str,
+                              edge_color: str, is_respawn: bool = False):
+    frequency_text: str = "Freq: {}".format(frequency)
+    if is_respawn:
+        e_color = edge_color + ':' + COLOR_RED
+        dot.edge(source_node, target_node, frequency_text, color=e_color, constraint='true', style='dashed')
+    else:
+        dot.edge(source_node, target_node, frequency_text, color=edge_color, constraint='true')
