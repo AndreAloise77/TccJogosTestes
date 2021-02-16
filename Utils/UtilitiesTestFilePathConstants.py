108211dc79af1159d520af299ecab320801cd99a
AndreAloise77 <44739910+AndreAloise77@users.noreply.github.com>
2021-02-16 00:48:44 -0300

    Updating FilePathCnstants
    Acrescentando constantes para o caminho dos arquivos de leitura do Gats e A-Gats, bem como a extensão dos arquivos lidos

--- Utils/UtilitiesTestFilePathConstants.py
+++ Utils/UtilitiesTestFilePathConstants.py
@@ -1,14 +1,23 @@
 class UtilitiesTestFilePathConstants:
 
-    # Constants
+    # SAVED FILE PATH
+    TEST_OUTPUT_PATH: str = 'test-output'
+    TEST_OUTPUT_GATS_PATH: str = 'gats'
+
+    # FILE EXTENSION
+    GRAPHVIZ_EXTENSION_FILE: str = '.gv'
+
+    # UNNAMED AREA
     PATH_01: str = 'C:/Users/silvi/Desktop/Angry Robot 3/Builds/Build 08_Data/Teste/02'
     PATH_02: str = 'C:/Users/silvi/Desktop/Angry Robot 3/Builds/Build 08_Data/Teste/01'
     PATH_03: str = 'C:/Users/silvi/Desktop/Angry Robot 3/Builds/Build 08_Data/Teste/03'
 
-    NAMED_AREA_PATH_01: str = 'C:/Users/silvi/Desktop/Angry Robot 3/ProvFiles/04'
-    NAMED_AREA_PATH_02: str = 'C:/Users/silvi/Desktop/Angry Robot 3/ProvFiles/05'
-    NAMED_AREA_PATH_03: str = 'C:/Users/silvi/Desktop/Angry Robot 3/ProvFiles/06'
-
+    # SESSION WITH BUG
     BUG_NAMED_PATH_01: str = 'C:/Users/silvi/Desktop/Angry Robot 3/ProvFiles/01'
     BUG_NAMED_PATH_02: str = 'C:/Users/silvi/Desktop/Angry Robot 3/ProvFiles/02'
     BUG_NAMED_PATH_03: str = 'C:/Users/silvi/Desktop/Angry Robot 3/ProvFiles/03'
+
+    # NAMED AREAS
+    NAMED_AREA_PATH_01: str = 'C:/Users/silvi/Desktop/Angry Robot 3/ProvFiles/04'
+    NAMED_AREA_PATH_02: str = 'C:/Users/silvi/Desktop/Angry Robot 3/ProvFiles/05'
+    NAMED_AREA_PATH_03: str = 'C:/Users/silvi/Desktop/Angry Robot 3/ProvFiles/06'
