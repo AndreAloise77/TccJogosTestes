ae7710ced22e8b2d0232f165c639905acab7f826
AndreAloise77 <44739910+AndreAloise77@users.noreply.github.com>
2021-02-16 12:19:03 -0300

    Adding new methods to UtilitiesIO
    Criando método para gerar uma árvore a partir de um nome de pasta
    
    Criando método que retorna uma lista com o nome de todos os arquivos dentro de uma pasta do diretório

--- Utils/UtilitiesIO.py
+++ Utils/UtilitiesIO.py
@@ -15,6 +15,23 @@ def get_single_file_in_dir(path: str):
     return tree
 
 
+def build_tree_by_filename(path: str):
+    tree = elementTree.parse(path)
+    return tree
+
+
+def get_fullname_from_all_files_in_dir(path: str):
+    absolute_path = os.path.abspath(path)
+    """print(absolute_path)"""
+    filename_list = []
+    for filename in os.listdir(absolute_path):
+        if not filename.endswith(FILE_TYPE_XML):
+            continue
+        fullname = os.path.join(absolute_path, filename)
+        filename_list.append(fullname)
+    return filename_list
+
+
 def get_all_files_in_dir_returns_list_element_tree(path: str):
     trees = []
     for filename in os.listdir(path):
