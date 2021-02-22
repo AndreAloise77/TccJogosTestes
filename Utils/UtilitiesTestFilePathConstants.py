3704acc7d10e40a99b1d51d624efef8b90c74e9a
AndreAloise77 <44739910+AndreAloise77@users.noreply.github.com>
2021-02-22 00:57:32 -0300

    Updating UtilitiesFipePathContants
    Adicionando constantes para o arquivo de arestas inválidas

--- Utils/UtilitiesTestFilePathConstants.py
+++ Utils/UtilitiesTestFilePathConstants.py
@@ -3,12 +3,18 @@ class UtilitiesTestFilePathConstants:
     # SAVED FILE PATH
     GATS_FORMAT_FILE_STRUCTURE: str = '{}/{}/{}/{}{}'
     AGATS_FORMAT_FILE_STRUCTURE: str = '{}/{}/{}{}'
+    INVALID_EDGES_FILES_STRUCTURE: str = '{}/{}{}'
     TEST_OUTPUT_PATH: str = 'test-output'
     TEST_OUTPUT_GATS_PATH: str = 'Gats'
     TEST_OUTPUT_AGATS_PATH: str = 'A-Gats'
+    INVALID_EDGES_FILES_DIRECTORY: str = 'Files/InvalidEdges'
+
+    # FILE NAME
+    INVALID_EDGE_FILENAME: str = 'invalid_edges'
 
     # FILE EXTENSION
     GRAPHVIZ_EXTENSION_FILE: str = '.gv'
+    TEXT_EXTENSION_FILE: str = '.txt'
 
     # UNNAMED AREA
     PATH_01: str = 'C:/Users/silvi/Desktop/Angry Robot 3/Builds/Build 08_Data/Teste/02'
