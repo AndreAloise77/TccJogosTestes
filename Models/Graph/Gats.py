1b0103dc6189c9b5402982e3d04e17fda476426a
AndreAloise77 <44739910+AndreAloise77@users.noreply.github.com>
2021-03-20 02:03:08 -0300

    Graph Models 01
    Realocando classe Gats para dentro da pasta Graph.
    
    Implementando método de exportação

---
+++ Models/Graph/Gats.py
@@ -0,0 +1,22 @@
+from typing import List
+
+from graphviz import Digraph
+
+from Models.Graph.Graph import Graph
+from Utils.UtilitiesFilePathConstants import UtilitiesFilePathConstants
+
+
+class Gats(Graph):
+    def __init__(self):
+        self.graph_gats: Graph = super().__init__()
+        self.sessions_list: List[str] = []
+        self.folder_name: str = ''
+
+    def set_current_time_to_folder(self, session_time: str):
+        self.folder_name = session_time
+
+    def export_file(self, path: str, file_name: str, gats: Digraph()):
+        gats.render(path.format(UtilitiesFilePathConstants.TEST_OUTPUT_PATH,
+                                UtilitiesFilePathConstants.OUTPUT_GATS_PATH,
+                                self.folder_name, file_name,
+                                UtilitiesFilePathConstants.GRAPHVIZ_EXTENSION_FILE), view=False)
