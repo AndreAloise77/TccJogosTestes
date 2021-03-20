0b0f2514b1394381cbb4489568e80372667c5a96
AndreAloise77 <44739910+AndreAloise77@users.noreply.github.com>
2021-03-20 02:22:04 -0300

    Creating UtilitiesFilePathConstants
    /criando classe utilitária para armazenar todas as constantes utilizadas referentes aos caminhos e arquivos externos

---
+++ Utils/UtilitiesFilePathConstants.py
@@ -0,0 +1,26 @@
+class UtilitiesFilePathConstants:
+
+    # FILE PATH STRUCTURE
+    GATS_FORMAT_FILE_STRUCTURE: str = '{}/{}/{}/{}{}'
+    AGATS_FORMAT_FILE_STRUCTURE: str = '{}/{}/{}/{}{}'
+    INVALID_EDGES_FILES_STRUCTURE: str = '{}/{}{}'
+    COMMON_EDGES_FILE_STRUCTURE: str = '{}/{}'
+
+    # DIRECTORIES
+    OUTPUT_PATH: str = 'test-output'
+    OUTPUT_GATS_PATH: str = 'Gats'
+    OUTPUT_AGATS_PATH: str = 'A-Gats'
+    TEST_OUTPUT_AGATS_PATH: str = 'A-Gats'
+    TEST_OUTPUT_PATH: str = 'test-output'
+    INVALID_EDGES_FILES_DIRECTORY: str = 'Files/InvalidEdges'
+    COMMON_EDGES_FILE_DIRECTORY: str = 'Files/CommonEdges'
+
+    # FILE NAME
+    INVALID_EDGE_FILENAME: str = 'invalid_edges'
+    COMMON_EDGES_FILENAME_TEMP: str = 'common_edges_temp.txt'
+    INVALID_EDGES_FILE_NAME: str = 'invalid_edges.txt'
+
+    # FILE EXTENSION
+    GRAPHVIZ_EXTENSION_FILE: str = '.gv'
+    TEXT_EXTENSION_FILE: str = '.txt'
+    XML_EXTENSION_FILE: str = '.xml'
