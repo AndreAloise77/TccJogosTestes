b79fa9dc645c1892554981d43303e96688af8734
AndreAloise77 <44739910+AndreAloise77@users.noreply.github.com>
2021-03-20 02:27:46 -0300

    Refactor UtilitiesProvConstants
    Refatorando constantes salvas

--- Utils/UtilitiesProvConstants.py
+++ Utils/UtilitiesProvConstants.py
@@ -1,18 +1,29 @@
 class UtilitiesProvConstants:
-    # Vertices:
-    VERTICES = 'vertices'
-    VERTEX = 'vertex'
-    ID = 'ID'
-    TYPE = 'type'
-    LABEL = 'label'
-    VERTEX_DATE = 'date'
-    ATTRIBUTES = 'attributes'
-    ATTRIBUTE = 'attribute'
-    ATTRIBUTE_NAME = 'name'
-    VALUE = 'value'
 
-    # Edge:
-    EDGES = 'edges'
-    EDGE = 'edge'
-    EDGE_SOURCE_ID = 'sourceID'
-    EDGE_TARGET_ID = 'targetID'
+    # Provenience Vertices:
+    VERTICES: str = 'vertices'
+    VERTEX: str = 'vertex'
+    ID: str = 'ID'
+    TYPE: str = 'type'
+    LABEL: str = 'label'
+    VERTEX_DATE: str = 'date'
+    ATTRIBUTES: str = 'attributes'
+    ATTRIBUTE: str = 'attribute'
+    ATTRIBUTE_NAME: str = 'name'
+    VALUE: str = 'value'
+
+    # Provenience Edge:
+    EDGES: str = 'edges'
+    EDGE: str = 'edge'
+    EDGE_SOURCE_ID: str = 'sourceID'
+    EDGE_TARGET_ID: str = 'targetID'
+
+    # Vertex Tags Names and Values
+    ATTRIBUTE_NAME_OBJECT_NAME: str = 'ObjectName'
+    ATTRIBUTE_VALUE_PLAYER: str = 'Player'
+    ACTIVITY: str = 'Activity'
+    RESPAWN = 'Respawn'
+    REGION = 'Region'
+
+    # Edge Tags Name Value
+    EDGE_LABEL_NEUTRAL = 'Neutral'
