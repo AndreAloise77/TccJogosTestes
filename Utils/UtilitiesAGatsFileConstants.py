8eae8241c654584bb531f3da54f37c8bf8093cc1
AndreAloise77 <44739910+AndreAloise77@users.noreply.github.com>
2021-03-20 02:19:35 -0300

    Creating UtilitiesAGatsFileConstants
    Criando classe utilitária para armazenar todas as constantes relacionadas com um AGats

---
+++ Utils/UtilitiesAGatsFileConstants.py
@@ -0,0 +1,23 @@
+class UtilitiesAGatsFileConstants:
+    # CONSTANTS TO READ AGATS FILE
+    DOUBLE_SLASH: str = '//'
+    DIGRAPH_WITH_OPEN_BRACES: str = 'digraph {'
+    CLOSE_BRACES: str = '}'
+    ARROW_SEPARATOR: str = '->'
+    HYPHEN_SEPARATOR: str = '-'
+    CONSTRAINT_WITH_EQUAL: str = 'constraint='
+
+    # CONSTANTS TO MANIPULATE AGATS FILE INFO
+    QUOTATION_MARK: str = '"'
+    DOUBLE_BACKSLASH: str = "\\"
+    JUMP_LINE: str = "\n"
+    WHITE_SPACE: str = ' '
+    RESPAWNED_INDICATOR: str = 'Resp'
+    OPEN_BRACKETS: str = '['
+    CLOSE_BRACKETS: str = ']'
+    QUOTATION_WITH_COLOR_EDGE: str = '" color='
+    COMMA_WITH_RESPAWNED_INDICATOR: str = ', Resp'
+    COLON: str = ':'
+    COMMON_EDGE_INDICATOR: str = 'color=blue'
+    INVALID_EDGE_INDICATOR: str = 'color=red'
+    ID_NODE_INDICATOR: str = 'Id:'
